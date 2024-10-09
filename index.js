a6bb7c3ac4a2fb803f36315315fcb283113426e0
Benjamin Yee <benyee1209@gmail.com>
2024-10-09 13:56:10 -0400

    s
    s

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ DOMSelectors.button.addEventListener("click", function(){
     console.log(event.target.parentElement);
     event.target.style.backgroundColor = "red";
 });
+
+
+// select all buttons as nodelist(can use foreach)
+const buttons = document.querySelectorAll("button");
+//make array from buttons if i want to use filter etc,
+const newButtons = Array.from(buttons);console.log(newButtons);
+//iiternate vthrough array and change each buttons color
+newButtons.forEach((button) => (button.style.backgroundColor));
+ newButtons.forEach((button)=> button.addEventListener("click", function(event){
+ console.log(event.target.textContent);
+ })
+);
