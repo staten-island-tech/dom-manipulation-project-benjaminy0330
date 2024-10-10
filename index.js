6c95e7c8aadea4ffc361422183c58e8b28e45121
Benjamin Yee <benyee1209@gmail.com>
2024-10-10 13:56:47 -0400

    d
    d

--- index.js
+++ index.js
@@ -19,6 +19,32 @@ const newButtons = Array.from(buttons);console.log(newButtons);
 //iiternate vthrough array and change each buttons color
 newButtons.forEach((button) => (button.style.backgroundColor));
  newButtons.forEach((button)=> button.addEventListener("click", function(event){
- console.log(event.target.textContent);
+ console.log(event.target.parentElement);
  })
 );
+
+let movie = {
+    title: "Star Wars",
+    release: 1977,
+};
+
+
+
+DOMSelectors.container.insertAdjacentHTML("beforeend",
+    `<div class ="card"> <h2 class ="card-header">${student}Some Text</h2></div>`
+);
+
+
+
+
+//1. create the html for inputs, cards, and containers
+
+//2. select/query the HTML form and get values from inputs
+
+//3. turn values into objects: Movie, Game, User, etc.
+
+//4. insert card w/ object onto screen onto HTML
+
+//5. add event lister for remove button in JS
+
+
