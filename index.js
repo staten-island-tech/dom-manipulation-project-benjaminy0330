e364f8e18e23104d2b6cef469c411fdd845c47b4
Benjamin <benyee1209@gmail.com>
2024-10-14 18:45:25 -0400

    s
    s

--- index.js
+++ index.js
@@ -2,17 +2,30 @@ const DOMSelectors = {
     button: document.getElementById("btn"),
     cardBox: document.getElementById("inputs"),  // Card container
     inputBox: document.getElementById("card-box"), // Input container
-    input: document.querySelector(`#input`),       // Input element
+    textInput: document.getElementById("input-text"),
+    imageInput: document.getElementById("input-image"),
+    clearbutton: document.getElementById("cbtn")
+
 };
 
 DOMSelectors.button.addEventListener("click", function(){
-    let inputValue = DOMSelectors.input.value; 
+    let imageUrl = DOMSelectors.imageInput.value;
+    let textValue = DOMSelectors.textInput.value; 
     let inputHTML = `
-        <div class="card">
-            ${inputValue}
+       <div class="card">
+            <h2> ${textValue} </h2>
+            <img src="${imageUrl}" alt="Image" class ="image">
+            
         </div>
+        `;
     
-    `;
     DOMSelectors.inputBox.insertAdjacentHTML("beforeend", inputHTML);
-    DOMSelectors.input.value = "";
+    DOMSelectors.textInput.value = "";
+    DOMSelectors.imageInput.value = "";
+
 });
+
+DOMSelectors.clearbutton.addEventListener("click", function() {
+    location.reload();
+})
+
