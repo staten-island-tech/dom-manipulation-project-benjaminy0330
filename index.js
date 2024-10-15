41430127ab3f459a7aeaed39df6102f53daa4cf9
Benjamin Yee <benyee1209@gmail.com>
2024-10-15 13:59:46 -0400

    sd
    sd

--- index.js
+++ index.js
@@ -2,28 +2,41 @@ const DOMSelectors = {
     button: document.getElementById("btn"), //submit button
     cardBox: document.getElementById("card-holder"),  //div where inserted material is added to html
     textInput: document.getElementById("input-text"), //text that is added
+    subInput: document.getElementById("input-sub"), //subtitle text that is added
     imageInput: document.getElementById("input-image"), //img url that is addedd
-    clearbutton: document.getElementById("cbtn") //button that clears page
+    clearbutton: document.getElementById("cbtn"), //button that clears page
+    deletebutton: document.getElementById("dbtn"),
 
 };
 
 DOMSelectors.button.addEventListener("click", function(){
     let imageUrl = DOMSelectors.imageInput.value;
     let textValue = DOMSelectors.textInput.value; 
+    let subValue = DOMSelectors.subInput.value;
     
     let inputHTML = `
        <div class="card">
             <h2> ${textValue} </h2>
-            <img src="${imageUrl}" alt="Image" class ="image">
+            <img src="${imageUrl}" alt="User inputed image" class ="image">
+            <p> ${subValue} </p>
+            
         </div>
         `;
     
     DOMSelectors.cardBox.insertAdjacentHTML("beforeend", inputHTML); 
     DOMSelectors.textInput.value = "";
+    DOMSelectors.subInput.value = "";
     DOMSelectors.imageInput.value = "";
 
 });
 
+/*DOMSelectors.deletebutton.addEventListener("click", function(){
+    h1.remove();
+});*/
+
 DOMSelectors.clearbutton.addEventListener("click", function() {
     location.reload(); //refreshes page to clear all created cards
 });
+
+//h1.remove();
+//<button type="button" id="dbtn">Remove</button>
