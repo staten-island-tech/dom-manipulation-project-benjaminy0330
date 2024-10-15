cd8f4225f73defe6e64dafc58173c27b9f0ae3b6
Benjamin <benyee1209@gmail.com>
2024-10-14 22:16:42 -0400

    s
    s

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
 const DOMSelectors = {
-    button: document.getElementById("btn"),
-    cardBox: document.getElementById("inputs"),  // Card container
-    inputBox: document.getElementById("card-box"), // Input container
-    textInput: document.getElementById("input-text"),
-    imageInput: document.getElementById("input-image"),
-    clearbutton: document.getElementById("cbtn")
+    button: document.getElementById("btn"), //submit button
+    cardBox: document.getElementById("inputs"),  //div where inputs are put
+    textInput: document.getElementById("input-text"), //text inputs
+    imageInput: document.getElementById("input-image"), //img url input
+    clearbutton: document.getElementById("cbtn") //clear button
 
 };
 
+//when click happens, code is inserted
 DOMSelectors.button.addEventListener("click", function(){
     let imageUrl = DOMSelectors.imageInput.value;
     let textValue = DOMSelectors.textInput.value; 
-    let inputHTML = `
+    let inputHTML = ` 
        <div class="card">
             <h2> ${textValue} </h2>
             <img src="${imageUrl}" alt="Image" class ="image">
             
         </div>
-        `;
+        `; //thing being inserted into html
     
     DOMSelectors.inputBox.insertAdjacentHTML("beforeend", inputHTML);
-    DOMSelectors.textInput.value = "";
+    DOMSelectors.textInput.value = ""; //leaves input box blank after use
     DOMSelectors.imageInput.value = "";
 
 });
 
 DOMSelectors.clearbutton.addEventListener("click", function() {
     location.reload();
-})
+}) //when button is clicked, it refreshes the page
+
+
+// ?? <button class="delete-btn">Delete</button>
 
